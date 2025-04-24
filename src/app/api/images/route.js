84672c8f67c0d77c74cf748dce9ff0848a617f6d
m-r-l-d <emeraldbay@comcast.net>
2025-04-24 11:10:31 -0500

    just use public bucket url

--- src/app/api/images/route.js
+++ src/app/api/images/route.js
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
-import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-const s3 = new S3Client({});
+const s3 = new S3Client({
+  
+});
 
 export async function GET() {
   try {
--- src/app/page.js
+++ src/app/page.js
@@ -2,45 +2,29 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-
-  const getImage = async () => {
-    try {
-      const response = await fetch('http://localhost:3000/api/images');
-      if (!response.ok) {
-        throw new Error('Failed to fetch image');
-      }
-      const data = await response.text()
-      return data
-    } catch (error) {
-      console.error('Error fetching image:', error);
-    }
-  }
-  
   async function ScreenshotImage() {
-   const imageUrl = await getImage()
-   
     return (
       <div>
-        {imageUrl ? (
-          <Image src={imageUrl} alt="s3url" width='1200' height='1200'/>
-        ) : (
-          <p>Loading image...</p>
-        )}
+        <Image
+          src={
+            "https://mrld-remote-img-test.s3.us-east-2.amazonaws.com/tools.png"
+          }
+          alt="s3url"
+          width="1200"
+          height="1200"
+        />
       </div>
     );
-  };
-
+  }
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-      <ScreenshotImage />
+        <ScreenshotImage />
         <ol>
-          <li>
-            Image above should come from s3 bucket.
-          </li>
+          <li>Image above should come from s3 bucket.</li>
         </ol>
-        </main>
+      </main>
     </div>
   );
 }
